ab5a7be6d3fce992b814f9f9d95719ded5443b54
AnahitGh <aghalmukhyan@mail.ru>
2018-08-01 15:47:50 +0400

    Add project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    constructor() {
+        const user = {
+            userName: 'An...',
+            surName: 'Gh...',
+            email: 'a...@mail.ru',
+            password: '******'
+        };
+        this.userName = user.userName;
+        this.surName = user.surName;
+        this.email = user.email;
+        this.password = user.password;
+    }
+
+    userName = 'An...';
+    surName = 'Gh...';
+    email = 'a...@mail.ru';
+    password = '******';
+
+    changeUserData() {
+        this.userName = "Anahit";
+        this.surName = "Ghalmukhyan";
+        this.email = "aghalmukhyan@mail.ru";
+        this.password = "111111";
+    }
 }
